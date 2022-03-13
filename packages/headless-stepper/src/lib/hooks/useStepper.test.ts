03312907085bbcf67a978575807b43b3484d8986
Rai Siqueira <rai93siqueira@gmail.com>
2022-03-13 20:15:59 -0300

    chore(lib): add tests

---
+++ packages/headless-stepper/src/lib/hooks/useStepper.test.ts
@@ -0,0 +1,55 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { Steps } from '../types';
+import { useStepper } from './useStepper';
+
+const steps: Steps[] = [
+  {
+    label: 'step 01',
+  },
+  {
+    label: 'step 02',
+  },
+];
+
+describe('useStepper', () => {
+  it('should start with 0 as current step and dont have previous step', () => {
+    const { result } = renderHook(({ steps }) => useStepper({ steps }), {
+      initialProps: { steps },
+    });
+    const { state } = result.current;
+    expect(state?.currentStep).toBe(0);
+    expect(state?.hasPreviousStep).toBeFalsy();
+  });
+
+  it('should start with 1 as current step and have previous step', () => {
+    const { result } = renderHook(
+      ({ steps, currentStep }) => useStepper({ steps, currentStep }),
+      {
+        initialProps: { steps, currentStep: 1 },
+      }
+    );
+    const { state } = result.current;
+    expect(state?.currentStep).toBe(1);
+    expect(state?.hasPreviousStep).toBeTruthy();
+  });
+
+  it('should update the current state to 1 and back to previous step', () => {
+    const { result } = renderHook(() => useStepper({ steps }));
+    const { state } = result.current;
+    expect(state?.currentStep).toBe(0);
+    expect(state?.hasPreviousStep).toBeFalsy();
+
+    act(() => {
+      result.current?.nextStep();
+    });
+
+    expect(result?.current?.state?.currentStep).toBe(1);
+    expect(result?.current?.state?.hasPreviousStep).toBeTruthy();
+
+    act(() => {
+      result.current?.prevStep();
+    });
+    expect(result?.current?.state?.currentStep).toBe(0);
+    expect(result?.current?.state?.hasPreviousStep).toBeFalsy();
+  });
+});
