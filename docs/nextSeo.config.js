4dba7ed7277720c4468a32a6c58f80d771dfc220
Rai Siqueira <rai93siqueira@gmail.com>
2022-03-16 17:53:51 -0300

    feat(docs): add next-seo

---
+++ docs/nextSeo.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: 'Headless Stepper',
+  titleTemplate: '%s | Headless Stepper',
+  defaultTitle: 'Headless Stepper',
+  description:
+    'Production ready React hook to create awesome stepper components. Effortless to use, easy to customize.',
+  canonical: 'https://headless-stepper.netlify.app',
+  openGraph: {
+    url: 'https://headless-stepper.netlify.app',
+    title: 'Headless Stepper',
+    description:
+      'Production ready React hook to create awesome stepper components. Effortless to use, easy to customize.',
+    images: [
+      {
+        url: '/GH_Cover.png',
+        alt: 'Headless Stepper og-image',
+      },
+    ],
+    site_name: 'Headless Stepper',
+  },
+  twitter: {
+    handle: '@rai_siqueira',
+    cardType: 'summary_large_image',
+  },
+};
+
+export default defaultSEOConfig;
