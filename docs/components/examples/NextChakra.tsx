a4e644e8a93456cd58425bbdf6e4a16b9bb4e7d8
Raí Siqueira <rai93siqueira@gmail.com>
2022-07-23 16:23:37 -0300

    feat(lib): upgrade to React 18
    closes #8
    
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

---
+++ docs/components/examples/NextChakra.tsx
@@ -0,0 +1,21 @@
+const SandBox = () => {
+  return (
+    <>
+      <iframe
+        src="https://codesandbox.io/embed/headless-stepper-nextjs-chakra-example-c8o134?fontsize=14&hidenavigation=1&theme=dark"
+        style={{
+          width: '100%',
+          height: '500px',
+          border: 0,
+          borderRadius: '4px',
+          overflow: 'hidden',
+        }}
+        title="headless-stepper-nextjs-chakra-example"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
+    </>
+  );
+};
+
+export default SandBox;
