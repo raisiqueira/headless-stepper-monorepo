30229e638642206e6c39725c51fe413a51215725
Raí Siqueira <rai93siqueira@gmail.com>
2022-09-17 16:47:12 -0300

    feat(lib): stepper component
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- packages/headless-stepper/src/index.ts
+++ packages/headless-stepper/src/index.ts
@@ -1,2 +1,6 @@
+import { Stepper, Step } from './lib/components/Stepper';
 // Public API for stepper.
 export * from './lib/hooks/useStepper';
+
+// Public API for stepper component.
+export { Stepper, Step };
