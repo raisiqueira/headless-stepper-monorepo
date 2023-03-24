739b43cf7e9133572ca5d2419286bed51c7ebf51
Raí Siqueira <rai93siqueira@gmail.com>
2023-03-23 21:34:11 -0300

    chore: update project dev dependencies
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- packages/headless-stepper/.storybook/main.js
+++ packages/headless-stepper/.storybook/main.js
@@ -10,7 +10,7 @@ module.exports = {
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [...rootMain.addons],
+  addons: ['@storybook/addon-essentials', ...rootMain.addons],
   webpackFinal: async (config, { configType }) => {
     if (rootMain.webpackFinal) {
       config = await rootMain.webpackFinal(config, { configType });
